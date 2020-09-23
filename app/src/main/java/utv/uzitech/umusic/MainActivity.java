aaaf0023c34f2322098022f93a15b852ae19e91a
Uzair Mukadam <uzair.mukadam1999@gmail.com>
2020-09-23 22:08:36 +0530

    Major code rewrite and changes.

--- app/src/main/java/utv/uzitech/umusic/MainActivity.java
+++ app/src/main/java/utv/uzitech/umusic/MainActivity.java
@@ -1,188 +1,107 @@
 package utv.uzitech.umusic;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.MediaMetadataRetriever;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.HorizontalScrollView;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import org.json.JSONObject;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
-    static ArrayList<JSONObject> allMusic;
-    ArrayList<String> allAlbum, allArtist;
-    static ArrayList<CardView> allCards;
+    ArrayList<JSONObject> allMusic;
+    ArrayList<String> allAlbums, allArtists, allFav;
 
-    HorizontalScrollView allTracks_view;
-
-    static int curr_pos = 0;
-
-    PlaybackService service;
-
-    static boolean inBackground = false;
-
-    BroadcastReceiver receiver;
+    DatabaseHelper database;
+    Common common;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        allTracks_view = findViewById(R.id.tracks_scrollView);
+        database = new DatabaseHelper(this);
+        common = new Common(getApplicationContext());
 
-        getPermission();
-        getMusic();
+        getData();
 
-        ImageView curr_art = findViewById(R.id.curr_artwork);
-        TextView curr_title = findViewById(R.id.curr_title);
-        TextView curr_artist = findViewById(R.id.curr_artist);
-        TextView curr_album = findViewById(R.id.curr_album);
-        ImageButton play_pause = findViewById(R.id.play_pause);
-        SeekBar seekBar = findViewById(R.id.curr_seekbar);
-
-        service = new PlaybackService(this, allMusic, curr_art, curr_title, curr_artist, curr_album, play_pause, seekBar);
-
-        setTracksList();
-
-        ImageButton playPrev = findViewById(R.id.play_prev);
-        ImageButton playNext = findViewById(R.id.play_next);
-
-        receiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String input = intent.getStringExtra("Remote_Input");
-                assert input != null;
-                switch (input) {
-                    case "BTN_PLAY":
-                        service.play(curr_pos);
-                        break;
-                    case "BTN_NEXT":
-                        service.playNext();
-                        actionRight(context);
-                        break;
-                    case "BTN_PREV":
-                        service.playPrev();
-                        actionLeft(context);
-                        break;
-                }
-                if(!inBackground){
-                    switch (input){
-                        case "D_RIGHT":
-                            actionRight(context);
-                            break;
-                        case "D_LEFT":
-                            actionLeft(context);
-                            break;
-                    }
-                }
-            }
-        };
-
-        playPrev.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                service.playPrev();
-                actionLeft(getApplicationContext());
-            }
-        });
-
-        playNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                service.playNext();
-                actionRight(getApplicationContext());
-            }
-        });
-
-        play_pause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                service.play(curr_pos);
-            }
-        });
-
-        highlightCard(getApplicationContext());
-
-        this.registerReceiver(receiver, new IntentFilter("utv.uzitech.remote_input"));
+        loadView();
     }
 
-    void actionRight(Context context) {
-        if (curr_pos != allMusic.size() - 1) {
-            curr_pos += 1;
-        } else {
-            curr_pos = 0;
-        }
-        highlightCard(context);
-    }
-
-    void actionLeft(Context context) {
-        if (curr_pos != 0) {
-            curr_pos -= 1;
-        } else {
-            curr_pos = allMusic.size() - 1;
-        }
-        highlightCard(context);
-    }
-
-    private void highlightCard(Context context) {
-        allTracks_view.smoothScrollTo(allCards.get(curr_pos).getLeft(), 0);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            for (int i = 0; i < allCards.size(); i++) {
-                CardView view = allCards.get(i);
-                if (i == curr_pos) {
-                    view.setCardBackgroundColor(context.getColor(R.color.colorAccent));
-                } else {
-                    view.setCardBackgroundColor(context.getColor(android.R.color.background_dark));
-                }
-            }
-        }
-    }
-
-    private void setTracksList() {
-        allCards = new ArrayList<>();
-        LinearLayout tracks_list = findViewById(R.id.tracks_list);
+    private void loadView() {
         float density = getResources().getDisplayMetrics().density;
+        allTrackList(density);
+        allAlbumList(density);
+        allArtistList(density);
+    }
 
-        for (int i = 0; i < allMusic.size(); i++) {
-            CardView view = createCard(allMusic.get(i), density, i);
-            allCards.add(view);
+    private void allArtistList(float density) {
+        LinearLayout tracks_list = findViewById(R.id.artists_list);
+
+        for (int i = 0; i < allArtists.size(); i++) {
+            CardView view = createCardA(allArtists.get(i), density, i);
             tracks_list.addView(view);
         }
     }
 
-    private CardView createCard(JSONObject object, float density, final int pos) {
+    private void allAlbumList(float density) {
+        LinearLayout tracks_list = findViewById(R.id.albums_list);
+
+        for (int i = 0; i < allAlbums.size(); i++) {
+            CardView view = createCardA(allAlbums.get(i), density, i);
+            tracks_list.addView(view);
+        }
+    }
+
+    private void allTrackList(float density){
+        LinearLayout tracks_list = findViewById(R.id.tracks_list);
+
+        for (int i = 0; i < allMusic.size(); i++) {
+            CardView view = createCard(allMusic.get(i), density, i);
+            tracks_list.addView(view);
+        }
+    }
+
+    private CardView createCardA(String name, float density, final int i) {
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        @SuppressLint("InflateParams") CardView layout = (CardView) inflater.inflate(R.layout.album_artist_card, null);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+
+        TextView title = layout.findViewById(R.id.track_title);
+
+        title.setText(name);
+
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), String.valueOf(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        params.setMargins((int) density * 8, 0, (int) density * 8, 0);
+        layout.setLayoutParams(params);
+
+        layout.setCardBackgroundColor(getResources().getColor(R.color.darkBackground));
+
+        return layout;
+    }
+
+    private CardView createCard(JSONObject object, float density, final int i) {
         LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         @SuppressLint("InflateParams") CardView layout = (CardView) inflater.inflate(R.layout.track_card, null);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
@@ -192,106 +111,87 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             title.setText(object.getString("title"));
-            if (object.has("artwork")) {
-                Bitmap bitmap = Bitmap.createScaledBitmap((Bitmap) object.get("artwork"), 500, 500, false);
-                artwork.setImageBitmap(bitmap);
-            }
+            Glide.with(getApplicationContext()).load(common.getMetadata(object.getString("source")).getEmbeddedPicture()).
+                    apply(new RequestOptions().override(500, 500)).
+                    placeholder(ContextCompat.getDrawable(getApplicationContext(), R.drawable.art_placeholder)).into(artwork);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setNowPlaying(i);
+            }
+        });
 
         params.setMargins((int) density * 8, 0, (int) density * 8, 0);
         layout.setLayoutParams(params);
 
-        layout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                curr_pos = pos;
-                service.play(curr_pos);
-                highlightCard(getApplicationContext());
-            }
-        });
+        layout.setCardBackgroundColor(getResources().getColor(R.color.darkBackground));
 
         return layout;
     }
 
-    private void getMusic() {
+    private void getData() {
         allMusic = new ArrayList<>();
-        allAlbum = new ArrayList<>();
-        allArtist = new ArrayList<>();
-
-        String sortOrder = MediaStore.MediaColumns.TITLE;
-
-        Cursor cursor = getApplicationContext().getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null, sortOrder);
-
-        Log.d("Time", "Start"); //
-        assert cursor != null;
-        while (cursor.moveToNext()) {
+        allAlbums = getIntent().getStringArrayListExtra("allAlbums");
+        allArtists = getIntent().getStringArrayListExtra("allArtists");
+        ArrayList<String> temp = getIntent().getStringArrayListExtra("allMusic");
+        assert temp != null;
+        for (String str : temp) {
             try {
-                JSONObject object = new JSONObject();
-                object.put("title", cursor.getString(cursor.getColumnIndex("title")));
-                object.put("album", cursor.getString(cursor.getColumnIndex("album")));
-                if (!allAlbum.contains(cursor.getString(cursor.getColumnIndex("album")))) {
-                    allAlbum.add(cursor.getString(cursor.getColumnIndex("album")));
-                }
-                object.put("artist", cursor.getString(cursor.getColumnIndex("artist")));
-                if (!allArtist.contains(cursor.getString(cursor.getColumnIndex("artist")))) {
-                    allArtist.add(cursor.getString(cursor.getColumnIndex("artist")));
-                }
-                object.put("source", cursor.getString(cursor.getColumnIndex("_data")));
-                object.put("artwork", getBitmap(cursor.getString(cursor.getColumnIndex("_data"))));
-                allMusic.add(object);
+                allMusic.add(new JSONObject(str));
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        Log.d("Time", "End"); //
-
-        cursor.close();
-
-        Collections.sort(allAlbum);
-        Collections.sort(allArtist);
+        checkFav();
+        Log.d("MAIN", "done");//
     }
 
-    private Bitmap getBitmap(String path) {
-        MediaMetadataRetriever metadataRetriever = new MediaMetadataRetriever();
-        Bitmap bitmap = null;
-        try {
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-                ParcelFileDescriptor fileDescriptor = getApplicationContext().getContentResolver().openFileDescriptor(Uri.fromFile(new File(path)), "r");
-                assert fileDescriptor != null;
-                FileDescriptor fileDescriptor1 = fileDescriptor.getFileDescriptor();
-                metadataRetriever.setDataSource(fileDescriptor1);
-            } else {
-                metadataRetriever.setDataSource(path);
+    private void checkFav() {
+        allFav = new ArrayList<>();
+        ArrayList<String> temp = database.getAllFav();
+        if(!temp.isEmpty()){
+            for(JSONObject object: allMusic){
+                try {
+                    if (temp.contains(object.getString("path"))) {
+                        temp.remove(object.getString("path"));
+                        allFav.add(object.getString("title"));
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
             }
-        } catch (Exception e) {
+            if(!temp.isEmpty()){
+                for(String path: temp){
+                    database.removeFav(path);
+                }
+            }
+        }else {
+            LinearLayout fav_sec = findViewById(R.id.fav_section);
+            fav_sec.setVisibility(View.GONE);
+        }
+    }
+
+    private void setNowPlaying(int pos){
+        ImageView artwork = findViewById(R.id.curr_artwork);
+        TextView title = findViewById(R.id.curr_title);
+        TextView artist = findViewById(R.id.curr_artist);
+        TextView album = findViewById(R.id.curr_album);
+
+        JSONObject object = allMusic.get(pos);
+
+        try {
+            Glide.with(getApplicationContext()).load(common.getMetadata(object.getString("source")).getEmbeddedPicture())
+                    .into(artwork);
+            title.setText(object.getString("title"));
+            artist.setText(object.getString("artist"));
+            album.setText(object.getString("album"));
+        }catch (Exception e){
             e.printStackTrace();
         }
-        if (metadataRetriever.getEmbeddedPicture() != null) {
-            InputStream inputStream = new ByteArrayInputStream(metadataRetriever.getEmbeddedPicture());
-            bitmap = BitmapFactory.decodeStream(inputStream);
-        }
-        return bitmap;
-    }
-
-    private void getPermission() {
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-            }
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        inBackground = true;
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        inBackground = false;
-        super.onResume();
     }
 }
