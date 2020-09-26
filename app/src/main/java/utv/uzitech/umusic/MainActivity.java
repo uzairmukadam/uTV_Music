291adf7f68f7fb8e0586d9c788fb1a25aba90f56
Uzair Mukadam <uzair.mukadam1999@gmail.com>
2020-09-26 19:37:45 +0530

    Fully working music player with shuffle and loop still pending.

--- app/src/main/java/utv/uzitech/umusic/MainActivity.java
+++ app/src/main/java/utv/uzitech/umusic/MainActivity.java
@@ -10,12 +10,15 @@ import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -35,6 +38,7 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,21 +46,27 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<JSONObject> allMusic;
     ArrayList<CardView> allCards;
-    //ArrayList<Integer> musicQueue;
     int[] tracks_tab;
 
+    FrameLayout fullScreen;
     LinearLayout allTracks_list;
     ScrollView allTracks_scroll;
 
-    ImageView curr_artwork;
-    TextView curr_title, curr_artist, curr_album;
-    SeekBar seekbar;
+    ImageButton play_pause, f_play_pause;
+
+    ImageView curr_artwork, full_artwork;
+    TextView curr_title, curr_artist, curr_album, full_title, full_details, full_curr, full_duration;
+    SeekBar seekbar, f_seekbar;
 
     BroadcastReceiver receiver, mediaplayback;
 
     MediaPlayer player;
 
     int track = 0, now_playing = -1;
+
+    Runnable runnable;
+
+    boolean loop = true, shuffle = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,11 +76,22 @@ public class MainActivity extends AppCompatActivity {
         allTracks_list = findViewById(R.id.tracks_parent);
         allTracks_scroll = findViewById(R.id.tracks_scroll);
 
+        fullScreen = findViewById(R.id.full_screen_playback);
+
         curr_artwork = findViewById(R.id.curr_artwork);
         curr_title = findViewById(R.id.curr_title);
         curr_artist = findViewById(R.id.curr_artist);
         curr_album = findViewById(R.id.curr_album);
         seekbar = findViewById(R.id.curr_seekbar);
+        play_pause = findViewById(R.id.play_pause);
+
+        full_artwork = findViewById(R.id.full_artwork);
+        full_title = findViewById(R.id.full_title);
+        full_details = findViewById(R.id.full_details);
+        full_curr = findViewById(R.id.full_curr_time);
+        full_duration = findViewById(R.id.full_duration);
+        f_seekbar = findViewById(R.id.full_seekbar);
+        f_play_pause = findViewById(R.id.full_play_pause);
 
         getAllFiles();
 
@@ -80,47 +101,81 @@ public class MainActivity extends AppCompatActivity {
 
         player = new MediaPlayer();
 
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                if (loop) {
+                    if (shuffle) {
+                        Play(new Random().nextInt(allMusic.size()));
+                    } else {
+                        Next();
+                    }
+                }
+            }
+        });
+
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 String input = intent.getStringExtra("Remote_Input");
                 assert input != null;
                 switch (input) {
+                    case "BTN_MENU":
+                        if (fullScreen.getVisibility() == View.GONE) {
+                            activateFullscreen();
+                        } else {
+                            deactivateFullscreen();
+                        }
+                        break;
                     case "BTN_BACK":
-                        MainActivity.super.onBackPressed();
+                        if (fullScreen.getVisibility() == View.GONE) {
+                            onBackPressed();
+                        } else {
+                            deactivateFullscreen();
+                        }
                         break;
                     case "D_DOWN":
-                        track += 4;
-                        if (track > allMusic.size() - 1) {
-                            if (track > allMusic.size() + tracks_tab[1] - 1) {
+                        if (fullScreen.getVisibility() == View.GONE) {
+                            track += 4;
+                            if (track > allMusic.size() - 1) {
+                                if (track > allMusic.size() + tracks_tab[1] - 1) {
+                                    track = 0;
+                                } else {
+                                    track = allMusic.size() - 1;
+                                }
+                            }
+                        }
+                        break;
+                    case "D_UP":
+                        if (fullScreen.getVisibility() == View.GONE) {
+                            track -= 4;
+                            if (track < 0) {
+                                track = allMusic.size() - 1;
+                            }
+                        }
+                        break;
+                    case "D_RIGHT":
+                        if (fullScreen.getVisibility() == View.GONE) {
+                            if (track != allMusic.size() - 1) {
+                                track += 1;
+                            } else {
                                 track = 0;
+                            }
+                        }
+                        break;
+                    case "D_LEFT":
+                        if (fullScreen.getVisibility() == View.GONE) {
+                            if (track != 0) {
+                                track -= 1;
                             } else {
                                 track = allMusic.size() - 1;
                             }
                         }
                         break;
-                    case "D_UP":
-                        track -= 4;
-                        if (track < 0) {
-                            track = allMusic.size() - 1;
-                        }
-                        break;
-                    case "D_RIGHT":
-                        if (track != allMusic.size() - 1) {
-                            track += 1;
-                        } else {
-                            track = 0;
-                        }
-                        break;
-                    case "D_LEFT":
-                        if (track != 0) {
-                            track -= 1;
-                        } else {
-                            track = allMusic.size() - 1;
-                        }
-                        break;
                     case "D_ENTER":
-                        Play(track);
+                        if (fullScreen.getVisibility() == View.GONE) {
+                            Play(track);
+                        }
                         break;
                 }
                 highlightCard();
@@ -145,6 +200,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
+        new Thread(runnable);
 
         registerReceiver(receiver, new IntentFilter("utv.uzitech.remote_input"));
         registerReceiver(mediaplayback, new IntentFilter("utv.uzitech.remote_input"));
@@ -283,7 +340,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    void activateFullscreen() {
+        findViewById(R.id.now_playing_panel).setVisibility(View.GONE);
+        findViewById(R.id.all_tracks_panel).setVisibility(View.GONE);
+        fullScreen.setVisibility(View.VISIBLE);
+    }
+
+    void deactivateFullscreen() {
+        fullScreen.setVisibility(View.GONE);
+        findViewById(R.id.now_playing_panel).setVisibility(View.VISIBLE);
+        findViewById(R.id.all_tracks_panel).setVisibility(View.VISIBLE);
+    }
+
     private void setSource(int i) {
+        seekbar.setProgress(0);
+        player.reset();
         try {
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
                 ParcelFileDescriptor fileDescriptor = getApplicationContext().getContentResolver().openFileDescriptor(Uri.fromFile(new File(allMusic.get(i).getString("source"))), "r");
@@ -294,11 +365,35 @@ public class MainActivity extends AppCompatActivity {
                 player.setDataSource(allMusic.get(i).getString("source"));
             }
             player.prepare();
-            seekbar.setMax(player.getDuration());
             setNowPlaying(i);
+            setAudioProgress();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void setAudioProgress() {
+        seekbar.setMax(player.getDuration());
+        f_seekbar.setMax(player.getDuration());
+        final Handler handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @SuppressLint({"DefaultLocale", "SetTextI18n"})
+            @Override
+            public void run() {
+                try {
+                    int mins = (player.getCurrentPosition() / 1000) / 60;
+                    int secs = (player.getCurrentPosition() / 1000) % 60;
+                    full_curr.setText(String.format("%02d", mins) + ":" + String.format("%02d", secs));
+                    seekbar.setProgress(player.getCurrentPosition());
+                    f_seekbar.setProgress(player.getCurrentPosition());
+                    handler.postDelayed(this, 1000);
+                } catch (IllegalStateException ed) {
+                    ed.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1000);
     }
 
     private void Play(int pos) {
@@ -308,15 +403,23 @@ public class MainActivity extends AppCompatActivity {
                 setSource(pos);
             }
             player.start();
+            play_pause.setImageResource(R.drawable.pause);
+            f_play_pause.setImageResource(R.drawable.pause);
         } else {
             if (pos != now_playing) {
                 player.stop();
+                play_pause.setImageResource(R.drawable.play);
+                f_play_pause.setImageResource(R.drawable.play);
                 player.reset();
                 now_playing = pos;
                 setSource(pos);
                 player.start();
+                play_pause.setImageResource(R.drawable.pause);
+                f_play_pause.setImageResource(R.drawable.pause);
             } else {
                 player.pause();
+                play_pause.setImageResource(R.drawable.play);
+                f_play_pause.setImageResource(R.drawable.play);
             }
         }
     }
@@ -341,13 +444,22 @@ public class MainActivity extends AppCompatActivity {
         cleanHiglight();
     }
 
+    @SuppressLint({"DefaultLocale", "SetTextI18n"})
     private void setNowPlaying(int pos) {
         try {
             curr_title.setText(allMusic.get(pos).getString("title"));
+            full_title.setText(allMusic.get(pos).getString("title"));
             curr_artist.setText(allMusic.get(pos).getString("artist"));
             curr_album.setText(allMusic.get(pos).getString("album"));
+            String details = allMusic.get(pos).getString("album") + " - " + allMusic.get(pos).getString("artist");
+            full_details.setText(details);
+            int mins = (player.getDuration() / 1000) / 60;
+            int secs = (player.getDuration() / 1000) % 60;
+            full_duration.setText(String.format("%02d", mins) + ":" + String.format("%02d", secs));
             Glide.with(getApplicationContext()).load(getMetadata(allMusic.get(pos).getString("source")).getEmbeddedPicture()).
                     placeholder(ContextCompat.getDrawable(getApplicationContext(), R.drawable.art_placeholder)).into(curr_artwork);
+            Glide.with(getApplicationContext()).load(getMetadata(allMusic.get(pos).getString("source")).getEmbeddedPicture()).
+                    placeholder(ContextCompat.getDrawable(getApplicationContext(), R.drawable.art_placeholder)).into(full_artwork);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -363,5 +475,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostResume() {
         registerReceiver(receiver, new IntentFilter("utv.uzitech.remote_input"));
         super.onPostResume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        player.release();
+        super.onBackPressed();
     }
 }
